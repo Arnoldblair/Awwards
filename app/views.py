f8481cc04921f8e132abfddfdcf5a642cf89230f
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-15 09:46:43 +0300

    add views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+
+# api
+from django.http import JsonResponse
+from rest_framework import status
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+from .permissions import IsAdminOrReadOnly
+
 
 # Create your views here.
