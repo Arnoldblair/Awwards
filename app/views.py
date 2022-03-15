d9a75765d3f45ab0c766f64d85799ce9b527211c
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-15 09:55:13 +0300

    edit views

--- app/views.py
+++ app/views.py
@@ -44,3 +44,27 @@ def profile(request):  # view profile
     profile = Profile.objects.filter(user_id=current_user.id).first()  # get profile
     project = Project.objects.filter(user_id=current_user.id).all()  # get all projects
     return render(request, "profile.html", {"profile": profile, "images": project})
+
+
+@login_required(login_url="/accounts/login/")
+def update_profile(request):
+    if request.method == "POST":
+
+        current_user = request.user
+
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        bio = request.POST["bio"]
+        contact = request.POST["contact"]
+
+        profile_image = request.FILES["profile_pic"]
+        profile_image = cloudinary.uploader.upload(profile_image)
+        profile_url = profile_image["url"]
+
+        user = User.objects.get(id=current_user.id)
+
+        # check if user exists in profile table and if not create a new profile
+        if Profile.objects.filter(user_id=current_user.id).exists():
