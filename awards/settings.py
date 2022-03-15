4e0439420f95f5dcc53429ecfceb839931a1f345
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-15 17:25:52 +0300

    configure cloudinary

--- awards/settings.py
+++ awards/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# cloudinary
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +31,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+
+cloudinary.config(
+  cloud_name = "dnlogkap2",
+  api_key = "445484296461758",
+  api_secret = "y0F_z0Vg55vo4Me7vlRrOf0ZKLY",
+)
 
 # Application definition
 
